00b5382fd560dae3782572e654b9dc4079b65785
diw28 <diw28@dimigo.hs.kr>
2023-11-15 14:01:12 +0900

    data directory move

--- raspberry_pi/merge.py
+++ raspberry_pi/merge.py
@@ -15,7 +15,7 @@ def merge(
     # img2 = cv2.imread('images/example2.jpg')
     # img3 = cv2.imread('images/example3.jpg')
     # img4 = cv2.imread('images/example4.jpg')
-    img_frame = cv2.imread(f"{PATH}/raspberry_pi/images/blackframe_big.png")
+    img_frame = cv2.imread(f"{PATH}/images/blackframe_big.png")
 
     # 이미지 크기를 맞춰줌
     img1_resized = cv2.resize(img1, (704*4, 528*4))
