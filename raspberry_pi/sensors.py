4a8b112f06cc849bfeba098101aaa2c0cbeb164d
dodo07070707 <aa01056328583@gmail.com>
2023-11-17 20:33:24 +0900

    basic code done, loop + pwm + led add

--- raspberry_pi/sensors.py
+++ raspberry_pi/sensors.py
@@ -19,8 +19,8 @@ GPIO.setup(segment_pins, GPIO.OUT)
 GPIO.setup(led_pin, GPIO.OUT)
 GPIO.setup(potentiometer_pin, GPIO.IN)
 
-pwm = GPIO.PWM(buzzer_pin, GPIO.OUT)
-pwm.start(50)
+buzzerpwm = GPIO.PWM(buzzer_pin, GPIO.OUT)
+buzzerpwm.start(50)
 
 # 7-Segment Display 디지트 맵
 digit_map = {
@@ -40,20 +40,24 @@ digit_map = {
 camera = PiCamera()
 camera.start_preview()  # OpenCV로 실시간 화면 보기
 
+#LED 초기화
+ledpwm = GPIO.PWM(led_pin, 100)  # PWM 주파수 설정
+ledpwm.start(0)  # PWM 시작, 초기 밝기 0으로 설정
+
 def display_digit(digit):
     for i, pin in enumerate(segment_pins):
         GPIO.output(pin, digit_map[digit][i])
 
 def buzzer_on():
-    pwm.ChangeFrequency(523) #도
+    buzzerpwm.ChangeFrequency(523) #도
     time.sleep(1)
-    pwm.ChangeFrequency(659) #미
+    buzzerpwm.ChangeFrequency(659) #미
     time.sleep(1)
-    pwm.ChangeFrequency(783) #솔
+    buzzerpwm.ChangeFrequency(783) #솔
     time.sleep(1)
 
 def buzzer_off():
-    pwm.ChangeFrequency(0)
+    buzzerpwm.ChangeFrequency(0)
 
 def take_photo():
     timestamp = time.strftime("%Y%m%d%H%M%S")
@@ -62,31 +66,40 @@ def take_photo():
     print(f"사진 촬영: {filename}")
 
 def main():
-    try:
-        start_time = time.time()
-        while True:
-            elapsed_time = time.time() - start_time
-            remaining_time = max(0, 120 - int(elapsed_time))
+    order_number = 0;
+    while True:
+        order_number+=1
+        try:
+            start_time = time.time()
+            while True:
+                #지난 시간 계산
+                elapsed_time = time.time() - start_time
+                remaining_time = max(0, 120 - int(elapsed_time))
+                
+                # 가변 저항 값 계산 + LED 조절
+                pot_value = GPIO.input(potentiometer_pin)
+                brightness = int(pot_value / 1023 * 100)  #정규화
+                ledpwm.ChangeDutyCycle(brightness)
+                
+                #7segment 활용하는 부분
+                if remaining_time >= 0 and remaining_time <= 9:
+                    display_digit(remaining_time // 10)
+                    time.sleep(0.1)
             
-            #7segment 활용하는 부분
-            if remaining_time >= 0 and remaining_time <= 9:
-                display_digit(remaining_time // 10)
-                time.sleep(0.1)
-           
-            # 120초가 지나거나 버튼이 눌리면 사진 찍기
-            if elapsed_time >= 120 or not GPIO.input(button_pin):
-                buzzer_on()
-                take_photo()
-                buzzer_off()
-                start_time = time.time()
+                # 120초가 지나거나 버튼이 눌리면 사진 찍기
+                if elapsed_time >= 120 or not GPIO.input(button_pin):
+                    buzzer_on()
+                    take_photo()
+                    buzzer_off()
+                    start_time = time.time()
 
-    except KeyboardInterrupt:
-        pass
+        except KeyboardInterrupt:
+            pass
 
-    finally:
-        camera.stop_preview()  # OpenCV로 실시간 화면 중지
-        GPIO.cleanup()
-        camera.close()
+        finally:
+            camera.stop_preview()  # OpenCV로 실시간 화면 중지
+            GPIO.cleanup()
+            camera.close()
 
 if __name__ == "__main__":
     main()
