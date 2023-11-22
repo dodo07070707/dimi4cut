719bea7fdc5f40b1f6ff9000269bf1f4cece2cd5
DOYOON KIM <98579912+dodo07070707@users.noreply.github.com>
2023-11-22 22:53:30 +0900

    bug fix, real simulation

--- raspberry_pi/sensors.py
+++ raspberry_pi/sensors.py
@@ -6,9 +6,8 @@ import numpy as np
 # GPIO 설정
 button_pin = 17  # 버튼 핀
 buzzer_pin = 18  # 부저 핀
-segment_pins = [21, 20, 16, 12, 25, 24, 23, 22]  # 7-Segment 핀
+segment_pins = (21, 20, 16, 12, 25, 24, 23, 22)  # 7-Segment 핀
 led_pin = 26  # LED 핀
-potentiometer_pin = 19  # 가변 저항 핀
 
 # GPIO 초기화
 g.setwarnings(False)
@@ -17,7 +16,6 @@ g.setup(button_pin, g.IN, pull_up_down=g.PUD_UP)
 g.setup(buzzer_pin, g.OUT)
 g.setup(segment_pins, g.OUT)
 g.setup(led_pin, g.OUT)
-g.setup(potentiometer_pin, g.IN)
 
 buzzerpwm = g.PWM(buzzer_pin, 1000)
 buzzerpwm.start(50)
@@ -36,27 +34,26 @@ digit_map = {
     '9' : (1, 1, 1, 1, 0, 1, 1, 0),
 }
 
-cv2.namedWindow("Life4Cut", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Life4Cut", 1280, 960)
 
 # 카메라 초기화
 cap = cv2.VideoCapture(0)  # OpenCV 카메라 객체 생성
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 960)
 
 # LED 초기화
 ledpwm = g.PWM(led_pin, 100)  # PWM 주파수 설정
 ledpwm.start(0)  # PWM 시작, 초기 밝기 0으로 설정
 
-segments = (2,3,4,5,6,7,8,9)
-
-for segment in segments:
+for segment in segment_pins:
     g.setup(segment, g.OUT)
     g.output(segment, False)
     
 
 def display_digit(digit):
+    print(digit)
     s = str(digit)
     for loop in range(0,8):
-        g.output(segments[loop], digit_map[s][loop])
+        g.output(segment_pins[loop], digit_map[s][loop])
 
 def buzzer_on():
     buzzerpwm.ChangeFrequency(523)  # 도
@@ -67,7 +64,7 @@ def buzzer_on():
     time.sleep(1)
 
 def buzzer_off():
-    buzzerpwm.ChangeFrequency(0)
+    buzzerpwm.ChangeFrequency(1)
 
 def take_photo():
     timestamp = time.strftime("%Y%m%d%H%M%S")
@@ -89,31 +86,28 @@ def main():
             while True:
                 # 지난 시간 계산
                 elapsed_time = time.time() - start_time
-                remaining_time = max(0, 120 - int(elapsed_time))
-                
-                # 가변 저항 값 계산 + LED 조절
-                pot_value = g.input(potentiometer_pin)
-                brightness = int(pot_value / 1023 * 100)  # 정규화
-                ledpwm.ChangeDutyCycle(brightness)
+                remaining_time = max(0, 20 - int(elapsed_time))
                 
                 # 7segment 활용하는 부분
                 if remaining_time >= 0 and remaining_time <= 9:
-                    display_digit(remaining_time // 10)
+                    display_digit(remaining_time)
                     time.sleep(0.1)
-            
+                    
+                print(elapsed_time, order_number)
                 # 120초가 지나거나 버튼이 눌리면 사진 찍기
-                if elapsed_time >= 120 or not g.input(button_pin):
+                if elapsed_time >= 20 or not g.input(button_pin):
                     buzzer_on()
                     take_photo()
                     buzzer_off()
-
+                    time.sleep(1)
+                    break
+                    
         except KeyboardInterrupt:
             pass
 
         finally:
             cap.release()  # 카메라 객체 해제
             cv2.destroyAllWindows()  # OpenCV 창 닫기
-            g.cleanup()
 
 if __name__ == "__main__":
     main()
